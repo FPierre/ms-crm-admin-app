4f312f212c10a5cf9f92ea75ca1d7a7a18a37c0b
pierre.flauder <pierre.flauder@seniormedia.fr>
2017-07-03 10:29:55 +0200

    Component de navigation

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,21 +1,37 @@
 <template>
-  <Menu mode='horizontal' theme='dark' active-name='1'>
-    <div class='layout-logo'></div>
+  <div class='app-nav-component'>
+    <nav class='nav has-shadow'>
+      <div class='container'>
+        <div class='nav-left'>
+          <a class='nav-item'>
+            <img src='http://bulma.io/images/bulma-logo.png' alt='Bulma logo'>
+          </a>
 
-    <div class='layout-nav'>
-      <Menu-item name='1'>
-        <router-link :to='{ name: "AgenciesIndex" }'>
-          <Icon type='ios-location'></Icon>
-          Agencies
-        </router-link>
-      </Menu-item>
+          <router-link class='nav-item is-hidden-mobile is-active' :to='{ name: "AgenciesIndex" }'>Home</router-link>
+          <router-link class='nav-item is-hidden-mobile' :to='{ name: "AgenciesIndex" }'>Agencies</router-link>
+          <router-link class='nav-item is-hidden-mobile' :to='{ name: "Logs" }'>Logs</router-link>
+        </div>
 
-      <Menu-item name='2'>
-        <router-link :to='{ name: "Logs" }'>
-          <Icon type='ios-analytics'></Icon>
-          Logs
-        </router-link>
-      </Menu-item>
-    </div>
-  </Menu>
+        <span class='nav-toggle'>
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+
+        <div class='nav-right nav-menu'>
+          <router-link class='nav-item is-hidden-tablet is-active' :to='{ name: "AgenciesIndex" }'>Home</router-link>
+          <router-link class='nav-item is-hidden-tablet' :to='{ name: "AgenciesIndex" }'>Agencies</router-link>
+          <router-link class='nav-item is-hidden-tablet' :to='{ name: "Logs" }'>Logs</router-link>
+
+          <a class='nav-item'>
+            <figure class='image is-16x16' style='margin-right: 8px;'>
+              <img src='http://bulma.io/images/jgthms.png'>
+            </figure>
+            Profile
+          </a>
+          <a class='nav-item'>Log out</a>
+        </div>
+      </div>
+    </nav>
+  </div>
 </template>
