4393665aa64fb371b91791fd8e231f9127a58fb1
pierre.flauder <pierre.flauder@seniormedia.fr>
2017-07-03 10:38:21 +0200

    Affichage de la liste des agencies

--- src/components/agency/AgenciesIndex.vue
+++ src/components/agency/AgenciesIndex.vue
@@ -1,20 +1,29 @@
 <template>
-  <Table :columns='columns1' :data='agencies'></Table>
+  <div class='agencies-index-component'>
+    <table class='table is-bordered is-striped'>
+      <thead>
+        <tr>
+          <th><abbr title='Identifiant'>ID</abbr></th>
+          <th>Name/th>
+          <th><abbr title='Commercial Status'>CS</abbr></th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for='{ id, name, commercialStatus } in agencies'>
+          <th>{{ id }}</th>
+          <td>{{ name }}</td>
+          <td>{{ commercialStatus }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 
 export default {
-  data () {
-    return {
-      columns1: [
-        { title: 'id', key: 'id' },
-        { title: 'name', key: 'name' },
-        { title: 'commercial status', key: 'commercialStatus' }
-      ]
-    }
-  },
   computed: mapGetters(['agencies']),
   created () {
     this.$store.dispatch('index')
