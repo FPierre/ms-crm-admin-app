352386fdc481c8f77cbf134a73b2c44f4359e2d6
pierre.flauder <pierre.flauder@seniormedia.fr>
2017-07-03 10:46:01 +0200

    Affichage de la liste des logs

--- src/components/Logs.vue
+++ src/components/Logs.vue
@@ -1,19 +1,33 @@
 <template>
-  <Table border :columns='columns6' :data='logs'></Table>
+  <div class='agencies-index-component'>
+    <div class='columns'>
+      <div class='column'>
+        <table class='table is-bordered is-striped'>
+          <thead>
+            <tr>
+              <th><abbr title='Identifiant'>ID</abbr></th>
+              <th>Event</th>
+              <th>Created at</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr v-for='{ id, event, createdAt } in logs'>
+              <th>{{ id }}</th>
+              <td>{{ event }}</td>
+              <td>{{ createdAt }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 
 export default {
-  data () {
-    return {
-      columns6: [
-        { title: 'event', key: 'event' },
-        { title: 'created at', key: 'createdAt' }
-      ]
-    }
-  },
   computed: mapGetters(['logs']),
   created () {
     this.$store.dispatch('update')
--- src/components/agency/AgenciesIndex.vue
+++ src/components/agency/AgenciesIndex.vue
@@ -1,22 +1,26 @@
 <template>
   <div class='agencies-index-component'>
-    <table class='table is-bordered is-striped'>
-      <thead>
-        <tr>
-          <th><abbr title='Identifiant'>ID</abbr></th>
-          <th>Name/th>
-          <th><abbr title='Commercial Status'>CS</abbr></th>
-        </tr>
-      </thead>
+    <div class='columns'>
+      <div class='column'>
+        <table class='table is-bordered is-striped'>
+          <thead>
+            <tr>
+              <th><abbr title='Identifiant'>ID</abbr></th>
+              <th>Name</th>
+              <th><abbr title='Commercial Status'>CS</abbr></th>
+            </tr>
+          </thead>
 
-      <tbody>
-        <tr v-for='{ id, name, commercialStatus } in agencies'>
-          <th>{{ id }}</th>
-          <td>{{ name }}</td>
-          <td>{{ commercialStatus }}</td>
-        </tr>
-      </tbody>
-    </table>
+          <tbody>
+            <tr v-for='{ id, name, commercialStatus } in agencies'>
+              <th>{{ id }}</th>
+              <td>{{ name }}</td>
+              <td>{{ commercialStatus }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
