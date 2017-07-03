3b7e1e33ef52753a39896ce925a32ac71ebdaae8
pierre.flauder <pierre.flauder@seniormedia.fr>
2017-07-03 14:15:00 +0200

    WIP show d'users

---
+++ src/components/user/UsersShow.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class='users-show-component'>
+    {{ id }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['id']
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Logs from '@/components/Logs'
 import Login from '@/components/Login'
 import AgenciesIndex from '@/components/agency/AgenciesIndex'
+import UsersShow from '@/components/user/UsersShow'
 import AgenciesNew from '@/components/agency/AgenciesNew'
 
 Vue.use(Router)
@@ -13,20 +14,23 @@ export default new Router({
     {
       path: '/',
       name: 'Login',
-      component: Login,
-      meta: { title: 'Login' }
+      component: Login
     },
     {
       path: '/agencies',
       name: 'AgenciesIndex',
-      components: { default: AgenciesIndex, modal: AgenciesNew },
-      meta: { title: 'Agencies' }
+      components: { default: AgenciesIndex, modal: AgenciesNew }
+    },
+    {
+      path: '/users/:id',
+      name: 'UsersShow',
+      components: UsersShow,
+      props: true
     },
     {
       path: '/logs',
       name: 'Logs',
-      components: { default: Logs },
-      meta: { title: 'Logs' }
+      components: { default: Logs }
     }
   ]
 })
