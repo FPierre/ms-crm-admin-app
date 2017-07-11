e891d65c556d1a16ea151ac05e03772a8606ec1f
pierre.flauder <pierre.flauder@seniormedia.fr>
2017-07-11 14:46:46 +0200

    WIP changement navbar

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,39 +1,54 @@
 <template>
   <div class='app-nav-component'>
-    <nav class='nav'>
-      <div class='container'>
-        <div class='nav-left'>
-          <a class='nav-item'>
-            <img src='http://bulma.io/images/bulma-logo.png' alt='Bulma logo'>
-          </a>
+    <nav class='navbar is-transparent'>
+      <div class='navbar-brand'>
+        <a class='navbar-item' href='http://bulma.io'>
+          <img src='http://bulma.io/images/bulma-logo.png' alt='Bulma: a modern CSS framework based on Flexbox' width='112' height='28'>
+        </a>
 
-          <router-link class='nav-item is-hidden-mobile is-active' :to="{ name: 'AgenciesIndex' }">Home</router-link>
-          <router-link class='nav-item is-hidden-mobile' :to="{ name: 'UsersIndex' }">Users</router-link>
-          <router-link class='nav-item is-hidden-mobile' :to="{ name: 'AgenciesIndex' }">Agencies</router-link>
-          <router-link class='nav-item is-hidden-mobile' :to="{ name: 'Logs' }">Logs</router-link>
-        </div>
-
-        <span class='nav-toggle'>
+        <div class='navbar-burger burger' data-target='navMenuExample'>
           <span></span>
           <span></span>
           <span></span>
-        </span>
-
-        <div class='nav-right nav-menu'>
-          <router-link class='nav-item is-hidden-tablet is-active' :to="{ name: 'AgenciesIndex' }">Home</router-link>
-          <router-link class='nav-item is-hidden-tablet' :to="{ name: 'UsersIndex' }">Users</router-link>
-          <router-link class='nav-item is-hidden-tablet' :to="{ name: 'AgenciesIndex' }">Agencies</router-link>
-          <router-link class='nav-item is-hidden-tablet' :to="{ name: 'Logs' }">Logs</router-link>
-
-          <a class='nav-item'>
-            <figure class='image is-16x16' style='margin-right: 8px;'>
-              <img src='http://bulma.io/images/jgthms.png'>
-            </figure>
-            Profile
-          </a>
-          <a class='nav-item'>Log out</a>
         </div>
       </div>
+
+      <div id='navMenuExample' class='navbar-menu'>
+        <div class='navbar-start'>
+          <router-link class='navbar-item is-hidden-mobile is-active'>
+            Home
+          </router-link>
+
+          <div class='navbar-item has-dropdown is-hoverable'>
+            <router-link :to="{ name: 'AgenciesIndex' }" class='navbar-link is-active'>
+              Agencies
+            </router-link>
+
+            <div class='navbar-dropdown'>
+              <router-link :to="{ name: 'AgenciesIndex' }" class='navbar-item is-active'>
+                List
+              </router-link>
+
+              <router-link :to="{ name: 'AgenciesNew' }" class='navbar-item'>
+                New
+              </router-link>
+
+              <router-link :to="{ name: 'AgenciesNew' }" class='navbar-item'>
+                Search
+              </router-link>
+
+              <hr class='navbar-divider'>
+
+              <div class='navbar-item'>
+                <div>version <p class='has-text-info'>0.4.3</p></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <router-link class='nav-item is-hidden-mobile' :to="{ name: 'UsersIndex' }">Users</router-link>
+      <router-link class='nav-item is-hidden-mobile' :to="{ name: 'Logs' }">Logs</router-link>
     </nav>
 
     <div class='tabs is-centered'>
