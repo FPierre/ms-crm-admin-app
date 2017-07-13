8532d485f2ddd4f2a1945063fb72d4bc7c5ce64c
pierre.flauder <pierre.flauder@seniormedia.fr>
2017-07-13 15:25:11 +0200

    WIP Timeline

---
+++ src/components/Timeline.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class='timeline-component'>
+    <div class='timeline' v-if='events'>
+      <div class='event' v-for='{ event, date, user } in events'>
+        <h1>{{ event }}</h1>
+        <h2>{{ date }}</h2>
+        <small>{{ userFullName }}</small>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['events']
+}
+</script>
+
+[
+  {
+    event: 'Event 1',
+    date: Date.now,
+    userFullName: ''
+  }
+]
