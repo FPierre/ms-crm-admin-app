c5743ead046f40588c7ee005a43ef0ba6650db15
pierre.flauder <pierre.flauder@seniormedia.fr>
2017-07-13 14:46:23 +0200

    Ajout du store pour leads

--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ import * as actions from './actions'
 import * as getters from './getters'
 import mutations from './mutations'
 import agencies from './modules/agencies'
+import leads from './modules/leads'
 import logs from './modules/logs'
 import users from './modules/users'
 
@@ -17,6 +18,7 @@ export default new Vuex.Store({
   mutations,
   modules: {
     agencies,
+    leads,
     logs,
     users
   },
