25cd042f0fbb83da28a6b44e9bdd702272ad07f6
FPierre <pflauder@gmail.com>
2017-07-16 21:10:41 +0200

    Simplification du HTML

--- src/components/agency/AgenciesIndex.vue
+++ src/components/agency/AgenciesIndex.vue
@@ -4,9 +4,7 @@
       <nav class='level'>
         <div class='level-left'>
           <div class='level-item'>
-            <p class='is-5'>
-              {{ total }} agencies
-            </p>
+            <p class='is-5'>{{ total }} agencies</p>
           </div>
 
           <div class='level-item'>
@@ -16,9 +14,7 @@
               </p>
 
               <p class='control'>
-                <button class='button'>
-                  Search
-                </button>
+                <button class='button'>Search</button>
               </p>
             </div>
           </div>
@@ -29,39 +25,35 @@
         </div>
       </nav>
 
-      <div class='columns'>
-        <div class='column'>
-          <table class='table is-bordered is-striped'>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Activities</th>
-                <th><abbr title='Commercial Status'>CS</abbr></th>
-                <th>Responsible</th>
-                <th>Created at</th>
-              </tr>
-            </thead>
+      <table class='table is-bordered is-striped'>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Activities</th>
+            <th><abbr title='Commercial Status'>CS</abbr></th>
+            <th>Responsible</th>
+            <th>Created at</th>
+          </tr>
+        </thead>
 
-            <tbody>
-              <tr v-for='{ _id, name, activities, commercialStatus, responsible, createdAt } in agencies'>
-                <td>
-                  <router-link :to="{ name: 'AgenciesShow', params: { id: _id }}">
-                  {{ name }}
-                  </router-link>
-                </td>
-                <td>{{ activities }}</td>
-                <td>{{ commercialStatus }}</td>
-                <td>
-                  <router-link :to="{ name: 'UsersShow', params: { id: responsible._id }}">
-                  {{ responsible.firstName }} {{ responsible.lastName }}
-                </router-link>
-               </td>
-               <td>{{ createdAt }}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
+        <tbody>
+          <tr v-for='{ _id, name, activities, commercialStatus, responsible, createdAt } in agencies'>
+            <td>
+              <router-link :to="{ name: 'AgenciesShow', params: { id: _id }}">
+                {{ name }}
+              </router-link>
+            </td>
+            <td>{{ activities }}</td>
+            <td>{{ commercialStatus }}</td>
+            <td>
+              <router-link :to="{ name: 'UsersShow', params: { id: responsible._id }}">
+                {{ responsible.firstName }} {{ responsible.lastName }}
+              </router-link>
+            </td>
+            <td>{{ createdAt }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
