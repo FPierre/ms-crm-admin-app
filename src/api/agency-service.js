20cdf80bb424646ab4a2b83d527eeb9ec00783fd
pierre.flauder <pierre.flauder@seniormedia.fr>
2017-07-17 18:12:26 +0200

    WIP call depuis l'app

--- src/api/agency-service.js
+++ src/api/agency-service.js
@@ -1,15 +1,38 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 
+import io from 'socket.io-client'
+
 Vue.use(VueResource)
 
 const resource = Vue.resource('http://localhost:4000/agencies{/id}')
 
 export default {
   index ({ page }, cb, errorCb) {
+    const socket = io.connect('http://localhost:5554/agencies')
+
+    // socket.on('connect', () => console.log('connected'))
+    console.log('index')
+
+    // setInterval(() => {
+    socket.emit('index', {}, data => {
+      console.log('index', data)
+      cb(data)
+    })
+    // }, 2000)
+
+    /*
+    socket.on('disconnect', err => {
+      console.log(err)
+      errorCb()
+    })
+    */
+
+    /*
     resource.query({ page })
       .then(res => cb(res.body))
       .catch(() => errorCb())
+    */
   },
 
   show ({ id }, cb, errorCb) {
