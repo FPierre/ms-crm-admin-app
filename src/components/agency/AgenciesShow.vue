e63720cf1f7d96778965d92e412989a628180bea
FPierre <pflauder@gmail.com>
2017-07-17 22:57:56 +0200

    Ajout des boutons mettre à jour et supprimer

--- src/components/agency/AgenciesShow.vue
+++ src/components/agency/AgenciesShow.vue
@@ -1,10 +1,30 @@
 <template>
   <div class='agencies-show-component'>
     <div class='container'>
+      <nav class='level'>
+        <div class='level-left'></div>
+
+        <div class='level-right'>
+          <p class='level-item'>
+            <router-link :to="{ name: 'AgenciesEdit', params: { id: agency._id } }" class='button is-outlined'>
+              Update
+            </router-link>
+          </p>
+
+          <p class='level-item'>
+            <router-link :to="{ name: 'AgenciesDelete', params: { id: agency._id } }" class='button is-danger is-outlined'>
+              Delete
+            </router-link>
+          </p>
+        </div>
+      </nav>
+
       <h1 class='title'>{{ agency.name }}</h1>
       <h2 class='subtitle'>{{ address }}</h2>
 
       <gmap-map :center='center' :zoom='6' style='height: 300px' :options='options'></gmap-map>
+
+      {{ h }}
     </div>
   </div>
 </template>
@@ -20,17 +40,19 @@ export default {
   },
   computed: {
     ...mapGetters({
-      agency: 'agencies/current'
+      agency: 'agencies/current',
+      h: 'agencies/history'
     }),
     center () {
       return { lat: this.agency.lat, lng: this.agency.lng }
     },
     address () {
-      return `${this.agency.street}`
+      return `${this.agency.street} ${this.agency.zipCode} ${this.agency.city}, ${this.agency.country}`
     }
   },
   created () {
     this.$store.dispatch('agencies/show', { id: this.$route.params.id })
+    this.$store.dispatch('agencies/history', { id: this.$route.params.id })
   }
 }
 </script>
