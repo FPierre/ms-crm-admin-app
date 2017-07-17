63d01d436a16b364c23b9e4cfd9a24c824d94198
pierre.flauder <pierre.flauder@seniormedia.fr>
2017-07-17 10:34:03 +0200

    Fix socket logs

--- src/api/log-service.js
+++ src/api/log-service.js
@@ -2,8 +2,18 @@ import io from 'socket.io-client'
 
 export default {
   stream (cb, errorCb) {
-    const socket = io('http://localhost:5555')
-    socket.on('logStream', logs => cb(logs))
-    socket.on('disconnect', errorCb())
+    const socket = io.connect('http://localhost:5555/monitoring')
+
+    socket.on('connect', () => console.log('connected'))
+
+    socket.on('statusUpdate', logs => {
+      console.log('statusUpdate')
+      cb(logs)
+    })
+
+    socket.on('disconnect', err => {
+      console.log(err)
+      errorCb()
+    })
   }
 }
