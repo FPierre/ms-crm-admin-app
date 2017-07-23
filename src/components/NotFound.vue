0fbc2546805e5a6a6b0885eaac10250bc5d02b75
FPierre <pflauder@gmail.com>
2017-07-23 17:35:51 +0200

    Page d'erreur

---
+++ src/components/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class='not-found-component'>
+    <section class='hero is-info is-fullheight'>
+      <div class='hero-body'>
+        <div class='container'>
+          <h1 class='title'>
+            Oups!
+          </h1>
+
+          <h2 class='subtitle'>
+            404
+          </h2>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
