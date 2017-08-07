7bbc037724d9b9a77261d5562d31584bf820db13
FPierre <pflauder@gmail.com>
2017-08-07 17:00:11 +0200

    Ajout de boxes

--- src/components/agency/AgenciesIndex.vue
+++ src/components/agency/AgenciesIndex.vue
@@ -1,63 +1,72 @@
 <template>
   <div class='agencies-index-component'>
     <div class='container'>
-      <nav class='level'>
-        <div class='level-left'>
-          <div class='level-item'>
-            <p class='is-5'>{{ total }} agencies</p>
-          </div>
-
-          <div class='level-item'>
-            <div class='field has-addons'>
-              <p class='control'>
-                <input type='text' class='input' placeholder='Search by name' v-model='searchQuery'>
-              </p>
-
-              <p class='control'>
-                <button class='button' @click='search'>Search</button>
-              </p>
+      <div class='box'>
+        <nav class='level'>
+          <div class='level-left'>
+            <div class='level-item has-text-centered'>
+              <div>
+                <p class='heading'>Agencies</p>
+                <p class='title'>{{ total }}</p>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div class='level-right'>
-          <pagination :page='page' :pages='pages' @navigate='navigate'></pagination>
-        </div>
-      </nav>
+          <div class='level-right'>
+            <div class='level-item'>
+              <div class='field has-addons'>
+                <p class='control'>
+                  <input type='text' class='input' placeholder='Search by name' v-model='searchQuery'>
+                </p>
 
-      <table class='table is-bordered is-striped' v-if='agencies.length'>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Activities</th>
-            <th><abbr title='Commercial Status'>CS</abbr></th>
-            <th>Responsible</th>
-            <th>Created at</th>
-          </tr>
-        </thead>
+                <p class='control'>
+                  <button class='button' @click='search'>Search</button>
+                </p>
+              </div>
+            </div>
 
-        <tbody>
-          <tr v-for='{ _id, name, activities, commercialStatus, responsible, createdAt } in agencies'>
-            <td>
-              <router-link :to="{ name: 'AgenciesShow', params: { id: _id }}">
-                {{ name }}
-              </router-link>
-            </td>
-            <td>{{ activities }}</td>
-            <td>{{ commercialStatus }}</td>
-            <td class='has-text-centered'>
-              <router-link :to="{ name: 'UsersShow', params: { id: _responsibleId }}" v-if='responsible'>
-                  {{ responsible.firstName }} {{ responsible.lastName }}
-              </router-link>
-              <vue-loader type='bubbles' color='#00d1b2' v-else></vue-loader>
-            </td>
-            <td>
-              {{ humanizedCreatedAt(createdAt) }}
-              <small>(<timeago :since='createdAt'></timeago>)</small>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+            <div class='level-item'>
+              <pagination :page='page' :pages='pages' @navigate='navigate'></pagination>
+            </div>
+          </div>
+        </nav>
+      </div>
+
+      <div class='box' v-if='agencies.length'>
+        <table class='table is-bordered is-striped'>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Activities</th>
+              <th><abbr title='Commercial Status'>CS</abbr></th>
+              <th>Responsible</th>
+              <th>Created at</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr v-for='{ _id, name, activities, commercialStatus, responsible, createdAt } in agencies'>
+              <td>
+                <router-link :to="{ name: 'AgenciesShow', params: { id: _id }}">
+                  {{ name }}
+                </router-link>
+              </td>
+              <td>{{ activities }}</td>
+              <td>{{ commercialStatus }}</td>
+              <td class='has-text-centered'>
+                <router-link :to="{ name: 'UsersShow', params: { id: _responsibleId }}" v-if='responsible'>
+                    {{ responsible.firstName }} {{ responsible.lastName }}
+                </router-link>
+                <vue-loader type='bubbles' color='#00d1b2' v-else></vue-loader>
+              </td>
+              <td>
+                {{ humanizedCreatedAt(createdAt) }}
+                <small>(<timeago :since='createdAt'></timeago>)</small>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
 </template>
@@ -108,3 +117,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.agencies-index-component {
+  padding: 3rem 0;
+}
+</style>
